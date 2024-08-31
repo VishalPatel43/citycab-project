b3a25d8766c85e36caf0ffc8ce4265350690347f
VishalPatel43 <202211012@daiict.ac.in>
2024-08-31 19:53:41 +0530

    Set up for cab booking project in Spring Boot

---
+++ citycab/src/main/java/com/springboot/project/citycab/CitycabApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project.citycab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitycabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitycabApplication.class, args);
+	}
+
+}
